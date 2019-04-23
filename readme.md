ce9ab596b5ad6381d780a1697c4fa04c449f1dce
Beatriz Pereira Do Valle Staub <beatrizpereiradovallestaub@Beatrizs-MacBook.local>
2019-04-23 00:38:05 -0400

    first commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+When I moved to the United Stated in August, I experienced a huge cultural shock, especially in regards to sex and gender. As crazy as it may sound, in Brazil, sex and the female body is much more of a taboo than in the US. I was struck by how here, women and men are much more acceptable with talking about sex and acting normal about it. Furthermore, I found the liberal feminist “free the nipple” movement very interesting especially because it would never be acceptable in my society. 
+
+With that in mind, I created the artwork, “Boobies”, to depict the freedom of sexuality in the United States.
+
+I was inspired by a Brazilian fashion designer, Adriana Degreas, that I admire and appreciate her work. 
---
+++ sketch.js
@@ -0,0 +1,56 @@
+function setup(){
+createCanvas (1100, 600);
+background(237, 29, 2);
+
+img = loadImage('images/body.png');
+img2 = loadImage ('images/hand1.png');
+img3 = loadImage ('images/hand2.png');
+img4 = loadImage ('images/boob1.png');
+img5 = loadImage ('images/boob2.png');
+img6 = loadImage ('images/rothand1.png');
+img7 = loadImage ('images/rothand2.png');
+
+}
+//drawn in iPad
+//images are all here
+
+function draw(){
+
+  background (237, 29, 2);
+
+  image(img, 0, 0,1200, 800);
+  (img, 50, 50, img.width/3, img.height/3);
+
+  image(img4, 250, 30, 300, 300);
+  (img4, 0, 0, img.width/3, img.height/3);
+
+  image(img5, 600, 30, 300, 300);
+  (img5, 0, 0, img.width/3, img.height/3);
+
+  (img2, 0, 0, img.width/3, img.height/3);
+  (img3, 0, 0, img.width/3, img.height/3);
+  (img6, 0, 0, img.width/3, img.height/3);
+  (img7, 0, 0, img.width/3, img.height/3);
+
+  fill(198, 119, 53);
+  ellipse(569, 590, 30, 40);
+
+  if (mouseIsPressed) {
+  
+    image(img6, -250, -190, 750, 650);
+    image(img7, 650, -190, 750, 650);
+  
+  }
+
+  else{
+    
+
+    image(img2, 400, 15, 700, 400);
+    image(img3, 0, 15, 700, 400);
+  }
+
+stroke(198, 119, 53);
+}
+
+//made by Beatriz Staub
+//inspired by Adriana Degreas
